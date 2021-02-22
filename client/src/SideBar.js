f1956cdf8a1d4bf1404ffe2a0827c71c28ea2e11
musaa-muhsen <musaamuhsen@gmail.com>
2021-02-22 23:19:57 +0000

    added loads of css

--- client/src/SideBar.js
+++ client/src/SideBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 export default function SideBar({single, setActive, isActive}) {
   //const [isActive, setActive] = useState(false);
   // console.log(single.imageSrc)
@@ -8,13 +9,36 @@ export default function SideBar({single, setActive, isActive}) {
  }
 // image 
 // name 
-//
+// Opening Hours: "hours"
+// vegan 
+// review 
   
     return (
         <div onClick={handleClick} className="overlay">
-               <img src={require(`./images/${single.imageSrc}`)} alt={single.imageSrc}/>
-                     <p>{single.name}</p>
-                     <button>read review</button>
+          <div className="sidebar-main-container">
+            <article class="sidebar-top-container">
+               <div className="make-center">
+                   <div className="close-container"><span  class="close-btn">
+                   <img aria-label="close" src={require('./images/x.svg')} alt={'close'}/>
+                     </span></div>
+                   <img   src={require(`./images/${single.imageSrc}`)} alt={single.imageSrc}/>
+                 </div>
+          
+               <ul className="sidebar-info-list">
+               <li>{single.name}</li>
+                     <li><span>Location: </span>{single.location}</li>
+                     <li><span>Opening Hours: </span><span>{single.hours}</span></li>
+                     <li><span>Vegan: </span><span>{single.vegan}</span></li>
+                     <li>
+                       <div className="reviewTitle">Review:</div>
+                       <div className="reviewDetails">{single.review}</div>
+                     </li>
+               </ul>
+               </article>
+            </div>
+           
+
+                    
         </div>
     )
 }
