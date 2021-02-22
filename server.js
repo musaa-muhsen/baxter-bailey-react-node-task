39fd8917c139ad2a0dd04a6d89b5b14ae06a99dd
musaa-muhsen <musaamuhsen@gmail.com>
2021-02-22 00:10:35 +0000

    changed server

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ const cors = require('cors');
 console.log(process.env.NODE_ENV)
 
 if (process.env.NODE_ENV === "production"){
-   app.use(express.static("build"));
+   app.use(express.static("client/build"));
    app.get("*", (req, res) => {
-     res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+     res.sendFile(path.resolve(__dirname, 'client',  "build", "index.html"));
    });
  }
 //console.log(cors)
