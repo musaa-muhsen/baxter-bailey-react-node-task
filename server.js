525046466494bb0962469f90be585a12e92d95d5
musaa-muhsen <musaamuhsen@gmail.com>
2021-02-22 00:26:56 +0000

    added some

--- server.js
+++ server.js
@@ -10,7 +10,13 @@ const cors = require('cors');
 //   response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 //   next();
 // });
-console.log(process.env.NODE_ENV)
+//console.log(process.env.NODE_ENV)
+//console.log(cors)
+app.use(cors())
+
+app.get("/api/", (request, response) => {
+   response.sendFile(__dirname + "/json/reviews.json");
+ });
 
 if (process.env.NODE_ENV === "production"){
    app.use(express.static("client/build"));
@@ -18,12 +24,7 @@ if (process.env.NODE_ENV === "production"){
      res.sendFile(path.resolve(__dirname, 'client',  "build", "index.html"));
    });
  }
-//console.log(cors)
-app.use(cors())
 
-app.get("/api/", (request, response) => {
-   response.sendFile(__dirname + "/json/reviews.json");
- });
 //app.get('/api/images', async (req, res) => {
 
 // app.get('/end', async (request, response) => {
